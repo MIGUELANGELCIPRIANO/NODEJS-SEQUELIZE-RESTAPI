0632a09cbfbeffc38a193057c9702e24126eced2
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-01-02 17:13:45 -0300

    [ADD]: Sequelize Setup

---
+++ src/database/database.js
@@ -0,0 +1,14 @@
+import Sequelize from 'sequelize'
+import 'dotenv/config'
+
+const { DB_NAME, DB_USER, DB_PASSWORD } = process.env
+
+export const sequelize = new Sequelize(
+	`${DB_NAME}`,
+	`${DB_USER}`,
+	`${DB_PASSWORD}`,
+	{
+		host: 'localhost',
+		dialect: 'postgres',
+	}
+)
