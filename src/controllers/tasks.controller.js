18cc5d81ee91a15022430be50a88266b1b232e72
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-01-11 17:13:30 -0300

    [ADD]: Tasks - CREATE/READ

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,24 @@
+import { Task } from '../models/Task.js'
+
+export const getTasks = async (req, res) => {
+	try {
+		const tasks = await Task.findAll()
+		res.json(tasks)
+	} catch (error) {
+		return res.status(500).json({ message: error.message })
+	}
+}
+
+export const createTask = async (req, res) => {
+	const { name, done, projectId } = req.body
+	try {
+		const newTask = await Task.create({
+			name,
+			done,
+			projectId,
+		})
+		res.json(newTask)
+	} catch (error) {
+		return res.status(500).json({ message: error.message })
+	}
+}
