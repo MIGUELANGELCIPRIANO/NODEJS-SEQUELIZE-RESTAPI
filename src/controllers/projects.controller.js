20f868f84ef1e91e03a982e204ed25d9eb19f35c
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-01-11 16:34:33 -0300

    [ADD]: Project - UPDATE/DELETE

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -9,17 +9,60 @@ export const getProjects = async (req, res) => {
 	}
 }
 
+export const getproject = async (req, res) => {
+	const { id } = req.params
+	try {
+		const project = await Project.findOne({
+			where: {
+				id,
+			},
+		})
+		if (!project)
+			return res.status(404).json({ message: 'Project does not exist' })
+		res.json(project)
+	} catch (error) {
+		return res.status(500).json({ message: error.message })
+	}
+}
+
 export const createProject = async (req, res) => {
 	const { name, priority, description } = req.body
-
 	try {
 		const newProject = await Project.create({
 			name,
 			description,
 			priority,
 		})
-
 		res.json(newProject)
+	} catch (error) {
+		return res.status(500).json({ message: error.message })
+	}
+}
+
+export const updateProject = async (req, res) => {
+	const { id } = req.params
+	const { name, priority, description } = req.body
+	try {
+		const project = await Project.findByPk(id)
+		project.priority = priority
+		project.name = name
+		project.description = description
+		await project.save()
+		res.json(project)
+	} catch (error) {
+		return res.status(500).json({ message: error.message })
+	}
+}
+
+export const deleteProject = async (req, res) => {
+	const { id } = req.params
+	try {
+		await Project.destroy({
+			where: {
+				id,
+			},
+		})
+		res.sendStatus(204)
 	} catch (error) {
 		return res.status(500).json({ message: error.message })
 	}
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,15 +1,18 @@
 import { Router } from 'express'
 import {
 	createProject,
+	deleteProject,
 	getProjects,
+	getproject,
+	updateProject,
 } from '../controllers/projects.controller.js'
 
 const router = Router()
 
 router.get('/projects', getProjects)
 router.post('/projects', createProject)
-router.put('/projects/:id')
-router.delete('/projects/:id')
-router.get('/projects/:id')
+router.put('/projects/:id', updateProject)
+router.delete('/projects/:id', deleteProject)
+router.get('/projects/:id', getproject)
 
 export default router
